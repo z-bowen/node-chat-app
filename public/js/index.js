9aeed2fefb3b68d915f5a45b43c824b9b11cbcd6
z-bowen <zach.bowen@gmail.com>
2018-11-15 17:45:00 +0100

    refactored time stamps to use moment

--- public/js/index.js
+++ public/js/index.js
@@ -10,17 +10,9 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  var date = new Date(message.createdAt);
-  var d = date.getDate();
-  var mon = date.getMonth() + 1;
-  var y = date.getFullYear();
-  var hour = date.getHours() - 1;
-  var h = (hour % 12) + 1;
-  var amPm = (hour < 12) ? 'am' : 'pm'
-  var m = date.getMinutes();
-  console.log(`New message from ${message.from} (${d}/${mon}/${y} ${h}:${m} ${amPm}): ${message.text}`);
+  console.log(`New message from ${message.from} (${message.createdAt}): ${message.text}`);
   var li = jQuery('<li></li>')
-  li.text(`New message from ${message.from} (${d}/${mon}/${y} ${h}:${m} ${amPm}): ${message.text}`)
+  li.text(`New message from ${message.from} (${message.createdAt}): ${message.text}`)
   jQuery('#messages').append(li);
 })
 
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,14 @@
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
-  return {from, text, createdAt: new Date()}
+  return {from, text, createdAt: moment().format('h:mm A DD/MM/YY')}
 }
 
 var generateLocationMessage = (from, lat, lng) => {
 
   var url = `https://www.google.com/maps/?q=${lat},${lng}`
 
-  return {from, url, createdAt: new Date()}
+  return {from, url, createdAt: moment().format('h:mm A DD/MM/YY')}
 }
 
 module.exports = {generateMessage, generateLocationMessage}
