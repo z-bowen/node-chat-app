dad221ad30a7ee755e15cca2e294d73a15b8bc01
z-bowen <zach.bowen@gmail.com>
2018-11-20 14:32:20 +0100

    made location private by room and added user names to message headers

--- server/server.js
+++ server/server.js
@@ -37,13 +37,19 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log(`New message from ${message.from}: ${message.text}`);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage("Admin", coords.lat, coords.lng))
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.lat, coords.lng))
+    }
   })
 
   socket.on('disconnect', () => {
